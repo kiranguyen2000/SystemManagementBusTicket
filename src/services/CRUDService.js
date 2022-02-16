04422a98fc68176a0c7a4c655049ec0e2a71505e
Nguyen Dang Hung <hungnguyendang2000@gmail.com>
2022-02-17 04:17:18 +0700

    Create function update data (Update)

--- src/services/CRUDService.js
+++ src/services/CRUDService.js
@@ -1,3 +1,4 @@
+import { reject } from 'bcrypt/promises';
 import bcrypt from 'bcryptjs';
 import db from '../models/index';
 
@@ -47,7 +48,53 @@ let getAllUser = () => {
         }
     })
 }
+
+let getUserInfoById = (userId) => {
+    return new Promise(async(resolve, reject) => {
+        try{
+            let user = await db.User.findOne({
+                where: {id: userId},
+                raw: true,
+            })
+            
+            if(user){
+                resolve(user)
+            }
+            else{
+                resolve({})
+            }
+        }catch(e){
+            reject(e);
+        }
+    })
+}
+
+let updateUserData = (data) => {
+    return new Promise (async(resolve,reject) => {
+        try{
+            let user = await db.User.findOne({
+                where: { id: data.id}
+            })
+            if(user){
+                user.firstName = data.firstName;
+                user.lastName = data.lastName;
+                user.address = data.address;
+                user.phonenumber = data.phonenumber;
+                await user.save();
+                let allUsers = await db.User.findAll();
+                resolve(allUsers);
+            }else{
+                resolve();
+            }
+        }catch(e){
+            reject(e)
+        }
+
+    })
+}
 module.exports = {
     createNewUser: createNewUser,
     getAllUser: getAllUser,
+    getUserInfoById: getUserInfoById,
+    updateUserData: updateUserData,
 }
